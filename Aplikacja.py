5e73086bc990eb969b58149f5c60c655cc63f26a
Matias0514 <63945390+Matias0514@users.noreply.github.com>
2023-05-18 13:23:21 +0200

    Add files via upload

---
+++ Aplikacja.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+class Aplikacja:
+    def __init__(self,okno):
+        self.text1 = tk.StringVar()
+        self.text2 = tk.StringVar()
+        self.text3 = tk.StringVar()
+        self.text4 = tk.StringVar()
+        self.text5 = tk.StringVar()
+        self.text6 = tk.StringVar()
+        self.text7 = tk.StringVar()
+        self.text8 = tk.StringVar()
+
+
+
+
+okno = tk.Tk()
+okno.title("NIE UKRADNIEMY TWOICH DANYCH ;)")
+okno.geometry("450x600")
+okno.resizable(False, False)
+okno.configure(background='black')
+okno.mainloop()
